14f2538895dee3d66c011332deab988c4cfb7bb8
neetscs <neetscs@gmail.com>
2013-05-26 13:50:41 -0700

    committed Hello world project

---
+++ src/edu/neetu/project1/Project1.java
@@ -0,0 +1,13 @@
+package edu.neetu.project1;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: neetu
+ * Date: 5/26/13
+ * Time: 1:40 PM
+ */
+public class Project1 {
+    public static void main(String[] args)  {
+        System.out.println("Hello World");
+    }
+}
