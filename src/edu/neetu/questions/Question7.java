aa30dd042bb10224d76bcf03dbb4528f3f32c02a
neetscs <neetscs@gmail.com>
2013-05-28 12:21:42 -0700

    solving question 7(modification)

--- src/edu/neetu/questions/Question7.java
+++ src/edu/neetu/questions/Question7.java
@@ -10,13 +10,16 @@ public class Question7 {
     
     public static void main(String[] args){
         
-        int x1 = 4, y1 = 4, height1 = 4, width1 = 4, x2 = 4, y2 = 4, height2 = 1, width2 = 1;
+        int x1 = 4, y1 = 4, height1 = 4, width1 = 4, x2 = 3, y2 = 3, height2 = 1, width2 = 1;
         
 
         boolean isOverlap1 = checkOverlap(x1, x2, width1, y1, y2, height1);
         boolean isOverlap2 = checkOverlap(x2, x1, width2, y2, y1, height2);
+        boolean isOverlap3 = checkOverlap(x2, x1, height1, y2, y1, width1);
+        boolean isOverlap4 = checkOverlap(x1, x2, height2, y2, y1, width2);
 
-        if (isOverlap1 || isOverlap2)
+
+        if (isOverlap1 || isOverlap2 || isOverlap3 || isOverlap4)
             System.out.println("The rectangles overlap.");
 
         else
