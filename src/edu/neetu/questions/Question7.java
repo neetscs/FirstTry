9128b77572cac304ae4370b2eb769677523bfcbc
neetscs <neetscs@gmail.com>
2013-05-28 16:41:51 -0700

    solving question 7(modification)

--- src/edu/neetu/questions/Question7.java
+++ src/edu/neetu/questions/Question7.java
@@ -10,8 +10,9 @@ public class Question7 {
     
     public static void main(String[] args){
 
-        int x1 =10, y1 = 10, height1 = 4, width1 = 4, x2 = 10, y2 = 11, height2 = 3, width2 = 3;
+        int x1 =10, y1 = 10, height1 = 4, width1 = 4, x2 = 10, y2 = 12, height2 = 1, width2 = 1;
 
+        //Check for overlap on each edge
         if (((x1 + width1) < x2) || ((x2 + width2) < x1) || ((y1 < y2 - height2) || (y2 < y1 - height1)))
             System.out.println("The rectangles do not overlap");
 
