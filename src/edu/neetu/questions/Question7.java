b447861f4cdfc6eb2cd0dd6b8ec4e28c6794126d
neetscs <neetscs@gmail.com>
2013-05-28 13:58:40 -0700

    solving question 7(modification)

--- src/edu/neetu/questions/Question7.java
+++ src/edu/neetu/questions/Question7.java
@@ -11,14 +11,14 @@ public class Question7 {
     public static void main(String[] args){
 
         int x1 = 4, y1 = 4, height1 = 4, width1 = 4, x2 = 9, y2 = 3, height2 = 1, width2 = 1;
+        int w1 = y1 - height1, z1 = x1 + width1, w2 = y2 - height2, z2 = x2 + width2;
 
-        boolean isOverlap1 = checkOverlap(x1, x2, width1, y1, y2, height1);
-        boolean isOverlap2 = checkOverlap(x2, x1, width2, y2, y1, height2);
-        boolean isOverlap3 = checkOverlap(x2, x1, height1, y2, y1, width1);
-        boolean isOverlap4 = checkOverlap(x1, x2, height2, y2, y1, width2);
+        boolean isOverlap1 = checkEachOverlap(z1, x2, z2, x1);
+
+        boolean isoverlap2 = checkEachOverlap(w1, y2, w2, y1);
 
 
-        if (isOverlap1 || isOverlap2 || isOverlap3 || isOverlap4)
+        if (isOverlap1 && isoverlap2)
             System.out.println("The rectangles overlap.");
 
         else
@@ -28,13 +28,10 @@ public class Question7 {
 
     }
 
-    private static boolean checkOverlap(int xCoordinate1, int xCoordinate2, int xCoordinate3, int yCoordinate1, int yCoordinate2, int yCoordinate3 ) {
-
-        if ((xCoordinate1 < xCoordinate2 && xCoordinate2 < xCoordinate3) && (yCoordinate1 < yCoordinate2 && yCoordinate2 < yCoordinate3))
-                return true;
-
+    private static boolean checkEachOverlap(int coordinate1, int coordinate2, int coordinate3, int coordinate4) {
+        if (coordinate1 > coordinate2 || coordinate3 < coordinate4)
+            return true;
 
         return false;
-
     }
 }
