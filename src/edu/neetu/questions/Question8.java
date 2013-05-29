cf3ed3e4241998abb650d79d71b51a996afbf59f
neetscs <neetscs@gmail.com>
2013-05-29 15:37:34 -0700

    solving question 8 - changes

--- src/edu/neetu/questions/Question8.java
+++ src/edu/neetu/questions/Question8.java
@@ -10,29 +10,19 @@ import java.util.Scanner;
  */
 public class Question8 {
 
+    private static final int MINUTE_UNITS_PER_HOUR = 5, TOTAL_DEGREES_PER_HOUR = 360, DEGREE_COVERED_BY_ONE_MINUTE = 6, TOTAL_NO_OF_MINUTES = 60, TOTAL_NO_OF_HOURS = 12;
+
     public static void main(String[] args){
 
         Scanner scanner = new Scanner(System.in);
-        float hours = 3, minutes = 15;
+        float hours = 2, minutes = 20;
         float angle = 0;
-        float noOfMinutes = 0;
 
-        //Calculate the number of minutes, noOfMinutes between hour hand and minute hand
+        //To find the total number of minutes between the hour hand and the minute hand.
+        float noOfMinutes = Math.abs(minutes - (MINUTE_UNITS_PER_HOUR * hours));
 
-        //If the hour hand is at 12
-        if (hours == 12)
-            noOfMinutes = minutes * 5;
-
-        //If the minute hand is at 12
-        else if (minutes == 0)
-            noOfMinutes = hours * 5;
-
-        //Other conditions
-        else
-            noOfMinutes = (Math.abs((minutes/5) - hours)) * 5;
-
-        //formula: noOfMinutes * 6 - (minutes/60 * 360/12)
-        angle = Math.abs(noOfMinutes * 6 - ((minutes * 30) / 60));
+        //to find the angle between the hour hand and minute hand.
+        angle = Math.abs((noOfMinutes * DEGREE_COVERED_BY_ONE_MINUTE) - ((minutes / TOTAL_NO_OF_MINUTES) * ( TOTAL_DEGREES_PER_HOUR / TOTAL_NO_OF_HOURS)));
 
         System.out.println(" The angle is: "+angle);
     }
