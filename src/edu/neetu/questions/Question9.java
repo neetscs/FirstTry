ebb5639f922733b25301e3e2c97088db4039d6e2
neetscs <neetscs@gmail.com>
2013-05-30 21:45:18 -0700

    commit changes till reading from index file and creating tree map

--- src/edu/neetu/questions/Question9.java
+++ src/edu/neetu/questions/Question9.java
@@ -1,5 +1,9 @@
 package edu.neetu.questions;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
 /**
  * Created with IntelliJ IDEA.
  * User: neetu
@@ -8,8 +12,40 @@ package edu.neetu.questions;
  */
 public class Question9 {
 
-    public static void main(String[] args){
+    private static TreeMap<Integer, ArrayList<Integer>> treeMap = new TreeMap();
 
+    public static void main(String[] args) {
 
+        String dataFileName = "data_file.txt";
+        String indexFileName = "index_file.txt";
+
+        try{
+            File indexFile = new File(indexFileName);
+            Scanner indexFileScanner =  new Scanner(indexFile);
+            while (indexFileScanner.hasNext()){
+                String eachIndex = indexFileScanner.next();
+                int colonLocation = eachIndex.indexOf(":");
+                int lineNumber = new Integer(eachIndex.substring(0, colonLocation));
+                int wordNumber = new Integer(eachIndex.substring(colonLocation + 1));
+
+                addWordNumberToMap(lineNumber, wordNumber);
+            }
+        }
+        catch(FileNotFoundException e){
+            System.out.println("File not found");
+        }
+    }
+
+    private static void addWordNumberToMap(int lineNumber, int wordNumber) {
+
+        ArrayList<Integer> wordNumberArray ;
+
+        if(treeMap.containsKey(lineNumber))
+            wordNumberArray = treeMap.get(lineNumber);
+        else
+            wordNumberArray = new ArrayList<Integer>();
+
+        wordNumberArray.add(wordNumber);
+        treeMap.put(lineNumber, wordNumberArray);
     }
 }
