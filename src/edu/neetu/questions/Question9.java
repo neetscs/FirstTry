3e527f9190877e4743b703afdf1efd41c50fe0e2
neetscs <neetscs@gmail.com>
2013-05-31 13:55:39 -0700

    committing the completed version prior to code cleanup

--- src/edu/neetu/questions/Question9.java
+++ src/edu/neetu/questions/Question9.java
@@ -13,30 +13,100 @@ import java.util.*;
 public class Question9 {
 
     private static TreeMap<Integer, ArrayList<Integer>> treeMap = new TreeMap();
+    private static HashMap<String, String> hashMap = new HashMap();
 
     public static void main(String[] args) {
+
+        int lineCounter = 0;
 
         String dataFileName = "data_file.txt";
         String indexFileName = "index_file.txt";
 
+        Scanner indexFileScanner, dataFileScanner;
+
+        File indexFile = new File(indexFileName);
+        File dataFile = new File(dataFileName);
+
         try{
-            File indexFile = new File(indexFileName);
-            Scanner indexFileScanner =  new Scanner(indexFile);
+
+            indexFileScanner = new Scanner(indexFile);
             while (indexFileScanner.hasNext()){
                 String eachIndex = indexFileScanner.next();
                 int colonLocation = eachIndex.indexOf(":");
                 int lineNumber = new Integer(eachIndex.substring(0, colonLocation));
                 int wordNumber = new Integer(eachIndex.substring(colonLocation + 1));
 
-                addWordNumberToMap(lineNumber, wordNumber);
+                //Scannin
+                addLineAndWordNumberToMap(lineNumber, wordNumber);
+            }
+
+            dataFileScanner = new Scanner(dataFile);
+
+            Set<Map.Entry<Integer, ArrayList<Integer>>> treeMapSet = treeMap.entrySet();
+            Iterator<Map.Entry<Integer, ArrayList<Integer>>> treeMapIterator = treeMapSet.iterator();
+
+            while (treeMapIterator.hasNext()){
+                Map.Entry<Integer, ArrayList<Integer>> mapEntry = treeMapIterator.next();
+                Integer lineNumber = mapEntry.getKey();
+                ArrayList<Integer> wordNumList = mapEntry.getValue();
+
+                while (lineCounter < lineNumber){
+                    if (lineCounter == lineNumber - 1)
+                        findWord(dataFileScanner, wordNumList, lineNumber);
+                    dataFileScanner.nextLine();
+                    lineCounter++;
+                }
+            }
+
+            indexFileScanner = new Scanner(indexFile);
+            String eachIndex;
+
+            while(indexFileScanner.hasNext()){
+                eachIndex = indexFileScanner.next();
+                printWord(eachIndex);
             }
         }
-        catch(FileNotFoundException e){
+
+        catch (FileNotFoundException e){
             System.out.println("File not found");
+        }
+
+    }
+
+    private static void printWord(String eachIndex) {
+        Set hashMapSet = hashMap.entrySet();
+        Iterator hashMapIterator = hashMapSet.iterator();
+
+        while (hashMapIterator.hasNext()){
+            Map.Entry mapEntry = (Map.Entry) hashMapIterator.next();
+            String keyInHashMap = (String) mapEntry.getKey();
+            String foundWord = (String) mapEntry.getValue();
+
+            if(eachIndex.equals(keyInHashMap)){
+                System.out.println(foundWord);
+            }
         }
     }
 
-    private static void addWordNumberToMap(int lineNumber, int wordNumber) {
+
+    private static void findWord(Scanner dataFileScanner, ArrayList<Integer> wordNumList, int lineNumber) {
+        int wordCounter = 0;
+        String result = null;
+        String wordInDataFile;
+        for (Integer wordToBeScannedPosition : wordNumList){
+
+            while (wordCounter < wordToBeScannedPosition) {
+                wordInDataFile = dataFileScanner.next();
+                if (wordCounter == wordToBeScannedPosition - 1){
+                    String foundWord = wordInDataFile;
+                    hashMap.put(String.valueOf(lineNumber) + ":" + String.valueOf(wordToBeScannedPosition), foundWord);
+                }
+                wordCounter++;
+            }
+        }
+    }
+
+    private static void addLineAndWordNumberToMap(int lineNumber, int wordNumber) {
 
         ArrayList<Integer> wordNumberArray ;
 
@@ -46,6 +116,7 @@ public class Question9 {
             wordNumberArray = new ArrayList<Integer>();
 
         wordNumberArray.add(wordNumber);
+        Collections.sort(wordNumberArray);
         treeMap.put(lineNumber, wordNumberArray);
     }
 }
