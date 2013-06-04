75d4dca7d6a1f1ed9926e6aa3acad3e17f27461c
neetscs <neetscs@gmail.com>
2013-06-04 12:59:25 -0700

    question 11 commit - using recursion

--- src/edu/neetu/questions/Question11.java
+++ src/edu/neetu/questions/Question11.java
@@ -10,19 +10,32 @@ import java.util.Scanner;
  */
 public class Question11 {
 
+    public static int sum = 0;
+
     public static void main(String[] args){
 
         Scanner scanner = new Scanner(System.in);
-        int sum = 0;
 
         System.out.println("Enter a number: ");
         int number = new Integer(scanner.next());
 
-        while( number != 0){
-            sum = sum + (number % 10);
-            number = number / 10;
-        }
+        sum = sumOfDigits(number);
 
         System.out.println("The sum of the numbers is: " + sum);
+    }
+
+    private static int sumOfDigits(int number) {
+
+       if (number == 0)
+           return sum;
+
+       else {
+           sum = sum + (number % 10);
+           number = number / 10;
+           sumOfDigits(number);
+       }
+
+     return sum;
+
     }
 }
